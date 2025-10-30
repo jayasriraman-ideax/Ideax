0da9dbea1d0af942e57002a53dc30a4726b4bab2
jayasriraman-ideax <jayasriraman.ideax@gmail.com>
2025-10-31 02:58:56 +0530

    Frontend Files

---
+++ backend/ideax-backend/src/main/java/com/example/ideax_backend/IdeaxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ideax_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaxBackendApplication.class, args);
+	}
+
+}
---
+++ frontend/ideax_frontend_app/android/app/src/main/kotlin/com/example/ideax_frontend_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ideax_frontend_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
